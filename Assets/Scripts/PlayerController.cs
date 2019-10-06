240a79bbc42b3b5db6b319437d1589a937fff2cf
daxaker <itjustsoap@gmail.com>
2019-10-06 22:22:56 +0300

    refactoring of Pull Request #8

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,29 +12,15 @@ public class PlayerController : MonoBehaviour
     public Space relativeTo = Space.World;
     private Transform _player;
 
-    private Animator animator;
-
-    Transform cam;
-    Vector3 camForward;
-    Vector3 move;
-    Vector3 moveInput;
-
-    float forwardAmount;
-    float turnAmount;
+    private Animator _animator;
+    private static readonly int Forward = Animator.StringToHash("forward");
+    private static readonly int Turn = Animator.StringToHash("turn");
 
     // Start is called before the first frame update
     void Start()
     {
         _player = gameObject.transform;
-        animator = this.GetComponentInChildren<Animator>();
-
-        cam = Camera.main.transform;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _animator = GetComponentInChildren<Animator>();
     }
 
     void Move(Vector3 move)
@@ -44,57 +30,33 @@ public class PlayerController : MonoBehaviour
             move.Normalize();
         }
 
-        this.moveInput = move;
-
-        ConvertMoveInput();
-        UpdateAnimator();
+        var res = transform.InverseTransformDirection(move);
+        UpdateAnimator(res.x, res.z);
+        _player.Translate(velocity * Time.fixedDeltaTime * move, relativeTo);
     }
 
-    void ConvertMoveInput()
+    void UpdateAnimator(float turnAmount, float forwardAmount)
     {
-        Vector3 localMove = transform.InverseTransformDirection(moveInput);
-        turnAmount = localMove.x;
-        forwardAmount = localMove.z;
-    }
-
-    void UpdateAnimator()
-    {
-        animator.SetFloat("forward", forwardAmount);//, 0.1f,Time.deltaTime);
-        animator.SetFloat("turn", turnAmount);//, 0.1f, Time.deltaTime);
+        _animator.SetFloat(Forward, forwardAmount);
+        _animator.SetFloat(Turn, turnAmount);
     }
 
     private void FixedUpdate()
     {
         var mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //print(mouse.ToString());
         mouse = new Vector3(mouse.x, 0, mouse.z); 
         _player.LookAt(mouse);
-
 
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-
-        if (cam != null)
-        {
-            camForward = Vector3.Scale(cam.up, new Vector3(1, 0, 1)).normalized;
-            move = vertical * camForward + horizontal * cam.right;
-        }
-        else
-        {
-            move = vertical * Vector3.forward + horizontal * Vector3.right;
-        }
-
-        if (move.magnitude > 1)
-        {
-            move.Normalize();
-        }
-
-        Move(move);
-
         Vector3 movement = new Vector3(horizontal, 0, vertical);
 
-
-        _player.Translate(velocity * Time.fixedDeltaTime * movement, relativeTo);
+        if (movement.magnitude > 1)
+        {
+            movement.Normalize();
+        }
+        
+        Move(movement);
     }
 }
